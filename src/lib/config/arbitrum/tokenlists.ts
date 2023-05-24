d13f2af12af104ee52feca5cc12e70a087b9b897
DaniyalAK47 <daniyalaka47@gmail.com>
2023-05-24 11:57:36 +0500

    updating the list to only include 2 tokens

--- src/lib/config/arbitrum/tokenlists.ts
+++ src/lib/config/arbitrum/tokenlists.ts
@@ -3,9 +3,11 @@ import { TokenListURLMap } from '@/types/TokenList';
 const tokenlists: TokenListURLMap = {
   Balancer: {
     Default:
-      'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/listed-old.tokenlist.json',
+      // 'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/listed-old.tokenlist.json',
+      'https://raw.githubusercontent.com/block360/frontend-v2/update-on-hompage/generated/listed-old.tokenlist.json',
     Vetted:
-      'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/balancer.tokenlist.json',
+      // 'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/balancer.tokenlist.json',
+      'https://raw.githubusercontent.com/block360/frontend-v2/update-on-hompage/generated/balancer.tokenlist.json',
   },
   External: [],
 };
--- src/lib/config/mainnet/tokenlists.ts
+++ src/lib/config/mainnet/tokenlists.ts
@@ -3,13 +3,15 @@ import { TokenListURLMap } from '@/types/TokenList';
 const tokenlists: TokenListURLMap = {
   Balancer: {
     Default:
-      'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/listed-old.tokenlist.json',
+      // 'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/listed-old.tokenlist.json',
+      'https://raw.githubusercontent.com/block360/frontend-v2/update-on-hompage/generated/listed-old.tokenlist.json',
     Vetted:
-      'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/balancer.tokenlist.json',
+      // 'https://raw.githubusercontent.com/balancer/tokenlists/main/generated/balancer.tokenlist.json',
+      'https://raw.githubusercontent.com/block360/frontend-v2/update-on-hompage/generated/balancer.tokenlist.json',
   },
   External: [
-    'ipns://tokens.uniswap.org',
-    'https://www.gemini.com/uniswap/manifest.json',
+    // 'ipns://tokens.uniswap.org',
+    // 'https://www.gemini.com/uniswap/manifest.json',
   ],
 };
 
